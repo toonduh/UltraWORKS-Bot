5c19af3b11fbb3700354ca475245121de94c72e1
toonduh <126681903+toonduh@users.noreply.github.com>
2026-07-17 21:49:57 -0400

    Update appointmap.ts

--- src/commands/appointmap.ts
+++ src/commands/appointmap.ts
@@ -1,1 +1,53 @@
+import {
+	SlashCommandBuilder
+} from "discord.js";
 
+import {
+	sendRobloxCommand
+} from "../utils/bridge.js";
+
+
+export default {
+
+	data:
+		new SlashCommandBuilder()
+			.setName("appointmap")
+			.setDescription(
+				"Appoints a Roblox map"
+			)
+
+			.addStringOption(option =>
+				option
+					.setName("map")
+					.setDescription("Map name")
+					.setRequired(true)
+			),
+
+
+	async execute(interaction:any){
+
+		await interaction.deferReply();
+
+
+		const map =
+			interaction.options.getString(
+				"map",
+				true
+			);
+
+
+		await sendRobloxCommand(
+			"appointmap",
+			{
+				map
+			}
+		);
+
+
+		await interaction.editReply(
+			`Appointed Roblox map: ${map}`
+		);
+
+	}
+
+};
