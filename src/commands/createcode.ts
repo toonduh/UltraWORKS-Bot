9f57c20e8eed68e5c387d20f203479730be6fd42
toonduh <126681903+toonduh@users.noreply.github.com>
2026-07-17 21:50:30 -0400

    Update createcode.ts

--- src/commands/createcode.ts
+++ src/commands/createcode.ts
@@ -1,1 +1,79 @@
+import {
+	SlashCommandBuilder
+} from "discord.js";
 
+
+import {
+	sendRobloxCommand
+} from "../utils/bridge.js";
+
+
+export default {
+
+	data:
+		new SlashCommandBuilder()
+		.setName("createcode")
+		.setDescription(
+			"Creates a redeemable code"
+		)
+
+		.addStringOption(o =>
+			o.setName("codename")
+			.setDescription("Code name")
+			.setRequired(true)
+		)
+
+		.addStringOption(o =>
+			o.setName("codetype")
+			.setDescription("Code type")
+			.setRequired(true)
+		)
+
+		.addStringOption(o =>
+			o.setName("award")
+			.setDescription("Award")
+			.setRequired(true)
+		)
+
+		.addIntegerOption(o =>
+			o.setName("amount")
+			.setDescription("Amount")
+			.setRequired(true)
+		),
+
+
+	async execute(interaction:any){
+
+		await interaction.deferReply();
+
+
+		const data = {
+
+			codename:
+				interaction.options.getString("codename"),
+
+			codetype:
+				interaction.options.getString("codetype"),
+
+			award:
+				interaction.options.getString("award"),
+
+			amount:
+				interaction.options.getInteger("amount")
+
+		};
+
+
+		await sendRobloxCommand(
+			"createcode",
+			data
+		);
+
+
+		await interaction.editReply(
+			"Code created."
+		);
+
+	}
+
+};
