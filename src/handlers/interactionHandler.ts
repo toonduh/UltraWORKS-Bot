015333d317406fc07db9e79a157dd410dadb4b3f
toonduh <126681903+toonduh@users.noreply.github.com>
2026-07-17 23:16:08 -0400

    Update interactionHandler.ts

--- src/handlers/interactionHandler.ts
+++ src/handlers/interactionHandler.ts
@@ -4,7 +4,7 @@ import ban from "../commands/ban.js";
 import createcode from "../commands/createcode.js";
 import clear from "../commands/clear.js";
 import gamestats from "../commands/gamestats.js";
-import find from "../commands/gamestats.js";
+import find from "../commands/find.js";
 
 
 
