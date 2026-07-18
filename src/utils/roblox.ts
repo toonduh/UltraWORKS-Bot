e62aa36e9d8c29d0f4c5510af359a253b6e8bd24
toonduh <126681903+toonduh@users.noreply.github.com>
2026-07-17 21:49:36 -0400

    Update roblox.ts

--- src/utils/roblox.ts
+++ src/utils/roblox.ts
@@ -1,1 +1,16 @@
+export async function getRobloxHeadshot(
+	userId:string
+){
 
+	const response = await fetch(
+		`https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=${userId}&size=420x420&format=Png`
+	);
+
+
+	const data:any =
+		await response.json();
+
+
+	return data.data?.[0]?.imageUrl;
+
+}
