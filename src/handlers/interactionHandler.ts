0fb5990f4d1f32d1cc2d124a64819373e1870176
toonduh <126681903+toonduh@users.noreply.github.com>
2026-07-17 21:48:14 -0400

    Update interactionHandler.ts

--- src/handlers/interactionHandler.ts
+++ src/handlers/interactionHandler.ts
@@ -1,1 +1,58 @@
+import about from "../commands/about.js";
+import appointmap from "../commands/appointmap.js";
+import ban from "../commands/ban.js";
+import createcode from "../commands/createcode.js";
 
+
+const commands = {
+	about,
+	appointmap,
+	ban,
+	createcode
+};
+
+
+export async function handleInteraction(
+	interaction:any
+){
+
+	if(!interaction.isChatInputCommand())
+		return;
+
+
+	const command =
+		commands[
+			interaction.commandName
+		];
+
+
+	if(!command)
+		return;
+
+
+	try {
+
+		await command.execute(
+			interaction
+		);
+
+	}
+	catch(error){
+
+		console.error(error);
+
+
+		if(interaction.deferred)
+		{
+			await interaction.editReply(
+				"Command failed."
+			);
+		}
+		else
+		{
+			await interaction.reply(
+				"Command failed."
+			);
+		}
+	}
+}
