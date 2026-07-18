5ae37532a1b813faf7a27f85026e806006c2cbe1
toonduh <126681903+toonduh@users.noreply.github.com>
2026-07-17 21:48:04 -0400

    Update registerCommands.ts

--- src/handlers/registerCommands.ts
+++ src/handlers/registerCommands.ts
@@ -1,1 +1,40 @@
+import {
+	REST,
+	Routes
+} from "discord.js";
 
+
+import about from "../commands/about.js";
+import appointmap from "../commands/appointmap.js";
+import ban from "../commands/ban.js";
+import createcode from "../commands/createcode.js";
+
+
+const commands = [
+	about.data,
+	appointmap.data,
+	ban.data,
+	createcode.data
+].map(
+	command => command.toJSON()
+);
+
+
+export async function registerCommands(
+	clientId:string
+){
+
+	const rest = new REST({
+		version:"10"
+	}).setToken(
+		process.env.DISCORD_TOKEN!
+	);
+
+
+	await rest.put(
+		Routes.applicationCommands(clientId),
+		{
+			body:commands
+		}
+	);
+}
