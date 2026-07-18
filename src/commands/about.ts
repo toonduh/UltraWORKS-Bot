f81d14ac85ea67eda511cac2f99cf19899cf3aec
toonduh <126681903+toonduh@users.noreply.github.com>
2026-07-17 21:49:47 -0400

    Update about.ts

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -1,1 +1,24 @@
+import {
+	SlashCommandBuilder
+} from "discord.js";
 
+
+export default {
+
+	data:
+		new SlashCommandBuilder()
+			.setName("about")
+			.setDescription(
+				"Shows information about UltraWORKS Bot"
+			),
+
+
+	async execute(interaction:any){
+
+		await interaction.reply(
+			"UltraWORKS Bot bridges Discord commands with Roblox games."
+		);
+
+	}
+
+};
