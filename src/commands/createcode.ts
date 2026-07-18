4931fa970894580a222c64792fb5480be05feba7
toonduh <126681903+toonduh@users.noreply.github.com>
2026-07-17 22:08:19 -0400

    Update createcode.ts

--- src/commands/createcode.ts
+++ src/commands/createcode.ts
@@ -1,79 +1,221 @@
 import {
-	SlashCommandBuilder
+	SlashCommandBuilder,
+	EmbedBuilder
 } from "discord.js";
 
+import { sendRobloxCommand } from "../utils/bridge.js";
 
-import {
-	sendRobloxCommand
-} from "../utils/bridge.js";
+const awardConfig: Record<string, {
+	image?: string;
+	color: number;
+}> = {
+	coins: {
+		image: "https://www.roblox.com/asset-thumbnail/image?assetId=119376087221654&width=420&height=420&format=png",
+		color: 0xFFD700
+	},
+
+	robux: {
+		color: 0x00A86B
+	},
+
+	item: {
+		color: 0x9B59B6
+	}
+};
 
 
 export default {
-
-	data:
-		new SlashCommandBuilder()
+	data: new SlashCommandBuilder()
 		.setName("createcode")
-		.setDescription(
-			"Creates a redeemable code"
+		.setDescription("Creates a redeemable code")
+
+		.addStringOption(option =>
+			option
+				.setName("codename")
+				.setDescription("Code name")
+				.setRequired(true)
 		)
 
-		.addStringOption(o =>
-			o.setName("codename")
-			.setDescription("Code name")
-			.setRequired(true)
+		.addStringOption(option =>
+			option
+				.setName("codetype")
+				.setDescription("Code type")
+				.setRequired(true)
 		)
 
-		.addStringOption(o =>
-			o.setName("codetype")
-			.setDescription("Code type")
-			.setRequired(true)
+		.addStringOption(option =>
+			option
+				.setName("award")
+				.setDescription("Award type")
+				.setRequired(true)
 		)
 
-		.addStringOption(o =>
-			o.setName("award")
-			.setDescription("Award")
-			.setRequired(true)
+		.addIntegerOption(option =>
+			option
+				.setName("amount")
+				.setDescription("Award amount")
+				.setRequired(true)
 		)
 
-		.addIntegerOption(o =>
-			o.setName("amount")
-			.setDescription("Amount")
-			.setRequired(true)
+		.addBooleanOption(option =>
+			option
+				.setName("onetime")
+				.setDescription("Can each player redeem once?")
+				.setRequired(true)
+		)
+
+		.addIntegerOption(option =>
+			option
+				.setName("stock")
+				.setDescription("Stock (-1 = infinite)")
+				.setRequired(true)
+		)
+
+		.addIntegerOption(option =>
+			option
+				.setName("starting")
+				.setDescription("Starting Unix timestamp")
+				.setRequired(true)
+		)
+
+		.addIntegerOption(option =>
+			option
+				.setName("expiration")
+				.setDescription("Expiration Unix timestamp")
+				.setRequired(true)
 		),
 
 
-	async execute(interaction:any){
+	async execute(interaction: any) {
 
 		await interaction.deferReply();
 
 
-		const data = {
+		const codename =
+			interaction.options.getString("codename", true);
 
-			codename:
-				interaction.options.getString("codename"),
+		const codetype =
+			interaction.options.getString("codetype", true);
 
-			codetype:
-				interaction.options.getString("codetype"),
+		const award =
+			interaction.options.getString("award", true)
+				.toLowerCase();
 
-			award:
-				interaction.options.getString("award"),
+		const amount =
+			interaction.options.getInteger("amount", true);
 
-			amount:
-				interaction.options.getInteger("amount")
+		const onetime =
+			interaction.options.getBoolean("onetime", true);
 
-		};
+		const stock =
+			interaction.options.getInteger("stock", true);
+
+		const starting =
+			interaction.options.getInteger("starting", true);
+
+		const expiration =
+			interaction.options.getInteger("expiration", true);
+
 
 
 		await sendRobloxCommand(
 			"createcode",
-			data
+			{
+				codename,
+				codetype,
+				award,
+				amount,
+				onetime,
+				stock,
+				starting,
+				expiration
+			}
 		);
 
 
-		await interaction.editReply(
-			"Code created."
-		);
 
+		const config =
+			awardConfig[award] ?? {
+				color: 0x5865F2
+			};
+
+
+
+		const embed =
+			new EmbedBuilder()
+				.setTitle("CODE CREATED")
+				.setDescription(
+					"A new redeemable code has been created."
+				)
+				.setColor(config.color)
+
+				.addFields(
+					{
+						name: "Code",
+						value: `\`${codename}\``,
+						inline: true
+					},
+
+					{
+						name: "Type",
+						value: codetype,
+						inline: true
+					},
+
+					{
+						name: "Award",
+						value: award,
+						inline: true
+					},
+
+					{
+						name: "Amount",
+						value: amount.toString(),
+						inline: true
+					},
+
+					{
+						name: "One Time Redeem",
+						value: onetime ? "Yes" : "No",
+						inline: true
+					},
+
+					{
+						name: "Stock",
+						value:
+							stock === -1
+								? "Infinite"
+								: stock.toString(),
+						inline: true
+					},
+
+					{
+						name: "Starts",
+						value: `<t:${starting}:F>`,
+						inline: false
+					},
+
+					{
+						name: "Expires",
+						value: `<t:${expiration}:F>`,
+						inline: false
+					}
+				)
+
+				.setTimestamp();
+
+
+
+		if (config.image) {
+			embed.setThumbnail(config.image);
+		}
+
+
+
+		await interaction.editReply({
+			embeds: [
+				embed
+			]
+		});
 	}
-
 };
